b1e7669478b3b9d647f3c5354e0d960de03bfe97
smamya <smamya@gmail.com>
2026-03-19 22:03:41 +0300

    DAVAI

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'prisma/config'
-import 'dotenv/config'
-
-export default defineConfig({
-  earlyAccessFeatures: {
-    driverAdapters: true,
-  },
-  schema: './prisma/schema.prisma',
-})
